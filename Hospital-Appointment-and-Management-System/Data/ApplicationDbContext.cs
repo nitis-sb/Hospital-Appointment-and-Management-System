e96c6019794cbaf8db0bc519d08a062d78081ef3
Nitis S B <2387561@cognizant.com>
2025-03-12 17:57:55 +0530

    [Nitis] - Doctor Schedule Db Context established with migrations

---
+++ Hospital-Appointment-and-Management-System/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Hospital_Appointment_and_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital_Appointment_and_Management_System.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+    }
+}
---
+++ Hospital-Appointment-and-Management-System/Models/DoctorSchedule.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_Appointment_and_Management_System.Models
+{
+    public class DoctorSchedule
+    {
+        [Key]
+        public required int DoctorID { get; set; }
+        public string AvailableTimeSlots { get; set; }
+    }
+}
