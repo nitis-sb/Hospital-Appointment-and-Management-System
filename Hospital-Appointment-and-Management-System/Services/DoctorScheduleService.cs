b0e3f328c97ba7547993aa2222a1f9aba22fc1c2
Nitis S B <2387561@cognizant.com>
2025-03-13 10:28:50 +0530

    [Nitis] - Implemented services for Doctor schedule

---
+++ Hospital-Appointment-and-Management-System/Services/DoctorScheduleService.cs
@@ -0,0 +1,25 @@
+﻿using Hospital_Appointment_and_Management_System.Models;
+using Hospital_Appointment_and_Management_System.Repositories;
+
+namespace Hospital_Appointment_and_Management_System.Services
+{
+    public class DoctorScheduleService
+    {
+        private readonly IDoctorScheduleRepository _repository;
+
+        public DoctorScheduleService(IDoctorScheduleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<DoctorSchedule> GetDoctorScheduleAsync(int doctorId)
+        {
+            return await _repository.GetDoctorScheduleAsync(doctorId);
+        }
+
+        public async Task UpdateDoctorScheduleAsync(DoctorSchedule schedule)
+        {
+            await _repository.UpdateDoctorScheduleAsync(schedule);
+        }
+    }
+}
